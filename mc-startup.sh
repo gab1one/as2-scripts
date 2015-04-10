96347c66a3c368fe879b54aad2f2070aeba5b4a2
Gabriel Einsdorf <gabriel.einsdorf@uni.kn>
2015-04-10 10:51:10 +0200

    fixed jar name in startup script

--- mc-startup.sh
+++ mc-startup.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-FORGE="./forge-1.7.10-10.13.2.1291-universal.jar"
+FORGE="./forge-1.7.10-10.13.3.1360-1.7.10-universal.jar"
 
 java -Xmx8g -Xms4g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -server -jar $FORGE nogui
